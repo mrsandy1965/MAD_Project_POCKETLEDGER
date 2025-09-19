d658f560bb47696b460d01c22f0ad1deacb0ada6
SANDESH LENDVE <123884782+mrsandy1965@users.noreply.github.com>
2025-09-19 15:29:19 +0000

    started frontend

---
+++ pocketledger-frontend/src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Login Screen</Text>
+    </SafeAreaView>
+  );
+}
